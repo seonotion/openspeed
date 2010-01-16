7f1ce68873ab3e8bc65d7c1b44a6b8be512e260c
vassdoki <vassdoki@cbaf95a6-fefa-11de-9748-a16e6a44286a>
2010-01-16 10:46:13 +0000

    The TextViews on the top got 50% width. Speed limit toggle buttons added.

--- trunk/src/hu/imind/android/openspeed/mainActivity.java
+++ trunk/src/hu/imind/android/openspeed/mainActivity.java
@@ -1,10 +1,10 @@
 package hu.imind.android.openspeed;
 
-import java.util.Formatter;
+import java.util.HashMap;
+import java.util.Iterator;
 
 import android.app.Activity;
 import android.graphics.Color;
-import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -12,48 +12,43 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class mainActivity extends Activity {
     /** Tag string for our debug logs */
     private static final String TAG = "DokiNspeed";
-    private int count = 0;
     private TextView tv1;
     private TextView tv2;
     private TextView tSpeed;
     private TextView gpsLat;
     private TextView gpsLon;
-    private Button button;
     private ViewGroup mainLayout;
-    private Formatter formatter;
-	private SensorManager mSensorManager;
 	private MyLocationListener mLocationListener;
 	private LocationManager mLocationManager;
+	private int currentSpeedLimit;
+	private int currentSpeed;
+	private HashMap<Integer, ToggleButton> buttons;
 
     public class MyLocationListener implements android.location.LocationListener {
 
 		@Override
 		public void onLocationChanged(Location location) {
-			// TODO Auto-generated method stub
 			updateGui(location);
 		}
 		
 		@Override
 		public void onProviderDisabled(String provider) {
-			// TODO Auto-generated method stub
             Log.d(TAG, "locationListener onProviderDisabled: " + provider);
 		}
 
 		@Override
 		public void onProviderEnabled(String provider) {
-			// TODO Auto-generated method stub
             Log.d(TAG, "locationListener onProviderEnabled: " + provider);
 		}
 
 		@Override
 		public void onStatusChanged(String provider, int status, Bundle extras) {
-			// TODO Auto-generated method stub
             Log.d(TAG, "locationListener onStatusChanged: " + provider + "status: " + status);
 		}
     	
@@ -63,41 +58,70 @@ public class mainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
         mainLayout = (ViewGroup)findViewById(R.id.mainLayout);
         tv1 = (TextView)findViewById(R.id.TextView01);
         tv2 = (TextView)findViewById(R.id.TextView02);
         tSpeed = (TextView)findViewById(R.id.speed);
         gpsLat = (TextView)findViewById(R.id.gpsLat);
         gpsLon = (TextView)findViewById(R.id.gpsLon);
-        button = (Button)this.findViewById(R.id.Button01);
+        
+        currentSpeedLimit = 50;
+        buttons  = new HashMap<Integer, ToggleButton>();
+        buttons.put(30,  (ToggleButton)findViewById(R.id.speedButton30));
+        buttons.put(50,  (ToggleButton)findViewById(R.id.speedButton50));
+        buttons.put(70,  (ToggleButton)findViewById(R.id.speedButton70));
+        buttons.put(90,  (ToggleButton)findViewById(R.id.speedButton90));
+        buttons.put(130, (ToggleButton)findViewById(R.id.speedButton130));
+
+        OnClickListener speedButtonListener = new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				ToggleButton tg = (ToggleButton)v;
+				setCurrentSpeedLimit(Integer.parseInt(tg.getTextOn().toString()));
+			}
+		};
+        for (Iterator i = buttons.values().iterator(); i.hasNext();) {
+			ToggleButton b = (ToggleButton) i.next();
+			b.setOnClickListener(speedButtonListener);
+		}
+        ToggleButton activeButton = buttons.get(currentSpeedLimit); 
+        if (activeButton != null) {
+        	activeButton.setChecked(true);
+        }
         
         mLocationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
         mLocationListener = new MyLocationListener();
         
-        button.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                count ++;
-                tv1.setText("Szia Ajna és helló világ!");
-                tv2.setText("Esemény: " + count);
-                Location l = new Location("gps");
-                l.setSpeed((float)34.16);
-                updateGui(l);
-            }
-        });
     }
+    
+    public void setCurrentSpeedLimit(int newSpeedLimit) {
+    	ToggleButton tg = buttons.get(currentSpeedLimit); 
+    	if (tg != null) {
+    		tg.setChecked(false);
+    	}
+    	currentSpeedLimit = newSpeedLimit;
+    	tg = buttons.get(currentSpeedLimit); 
+    	if (tg != null) {
+    		tg.setChecked(true);
+    	}
+    	updateGui(null);
+    }
+    
     public void updateGui(Location location) {
-		tv1.setText("Accuracy: " + location.getAccuracy());
-		tv2.setText("Altitude: " + location.getAltitude());
-		gpsLat.setText(Double.toString(location.getLatitude()));
-		gpsLon.setText(Double.toString(location.getLongitude()));
-		tSpeed.setText(formatSpeed(location.getSpeed()));
-		int speed = Math.round(location.getSpeed() * (float)3.6);
+    	if (location != null) {
+			tv1.setText("Accuracy: " + location.getAccuracy());
+			tv2.setText("Altitude: " + location.getAltitude());
+			gpsLat.setText(Double.toString(location.getLatitude()));
+			gpsLon.setText(Double.toString(location.getLongitude()));
+			tSpeed.setText(formatSpeed(location.getSpeed()));
+			currentSpeed = Math.round(location.getSpeed() * (float)3.6);
+    	}
 		//tSpeed.setBackgroundColor(Color.RED);
-		if (speed > 60) {
+		if (currentSpeed > currentSpeedLimit + 10) {
 			mainLayout.setBackgroundColor(Color.RED);
 			tSpeed.setTextColor(Color.BLACK);
-		} else if (speed > 50) {
+		} else if (currentSpeed >= currentSpeedLimit) {
 			//mainLayout.setBackgroundColor(R.color.speeding_1);
 			mainLayout.setBackgroundColor(Color.GREEN);
 			tSpeed.setTextColor(Color.BLACK);
@@ -106,7 +130,7 @@ public class mainActivity extends Activity {
 			mainLayout.setBackgroundColor(Color.BLACK);
 			tSpeed.setTextColor(Color.RED);
 		}
-        Log.d(TAG, "locationListener onLocationChanged: " + location);
+        //Log.d(TAG, "locationListener onLocationChanged: " + location);
 	}
 
     private String formatSpeed(float speed) {
